523c4bd0b50037c38c758ca92823331223e5edb0
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2025-02-05 11:37:34 +0100

    Update laplaceGNN.py

--- laplaceGNN/laplaceGNN.py
+++ laplaceGNN/laplaceGNN.py
@@ -3,17 +3,154 @@ import torch
 import torch_geometric
 import torch_geometric.nn as pyg_nn  
 
-class LaplaceGNN(torch.nn.Module):
+class LaplaceGNN_v1(torch.nn.Module):
     r""" LaplaceGNN: LaplaceGNN: Scalable Graph Learning through Spectral Bootstrapping and Adversarial Training
     Args:
-        encoder1 (torch.nn.Module): First encoder network for augmentations (from module.py).
-        encoder2 (torch.nn.Module): Second encoder network for augmentations (from module.py).
+        encoder (torch.nn.Module): Encoder network to be duplicated and used in both online and target networks.
+        predictor (torch.nn.Module): Predictor network used to predict the target projection from the online projection.
+
+    .. note::
+        encoder must have a `reset_parameters` method, as the weights of the target network will be initialized
+        differently from the online network.
+    """
+    def __init__(self, encoder, predictor):
+        super().__init__()
+        # online network
+        self.online_encoder = encoder
+        self.predictor = predictor
+
+        # target network
+        self.target_encoder = copy.deepcopy(encoder)
+        # reinitialize weights
+        self.target_encoder.reset_parameters()
+        # stop gradient
+        for param in self.target_encoder.parameters():
+            param.requires_grad = False
+
+    def trainable_parameters(self):
+        r"""Returns the parameters that will be updated via an optimizer."""
+        return list(self.online_encoder.parameters()) + list(self.predictor.parameters())
+
+    @torch.no_grad()
+    def update_target_network(self, mm, centering=True, sharpening=True, center_momentum=0.9, temperature=0.04):
+        r"""Performs a momentum update of the target network's weights, with optional centering and sharpening.
+
+        Args:
+            mm (float): Momentum used in moving average update.
+            centering (bool): Whether to apply centering to the target network outputs.
+            sharpening (bool): Whether to apply sharpening to the target network outputs.
+            center_momentum (float): Momentum used for updating the center.
+        """
+        assert 0.0 <= mm <= 1.0, "Momentum needs to be between 0.0 and 1.0, got %.5f" % mm
+        assert 0.0 <= center_momentum <= 1.0, "Center momentum needs to be between 0.0 and 1.0, got %.5f" % center_momentum
+
+        # Momentum update of the target network's weights
+        for param_q, param_k in zip(self.online_encoder.parameters(), self.target_encoder.parameters()):
+            param_k.data.mul_(mm).add_(param_q.data, alpha=1. - mm)
+
+        if centering:
+            # Initialize center if not already done
+            if not hasattr(self, 'center'):
+                self.center = torch.zeros_like(next(self.target_encoder.parameters()))
+
+            # Update center with exponential moving average
+            with torch.no_grad():
+                for param in self.target_encoder.parameters():
+                    self.center.mul_(center_momentum).add_(param.data.mean(), alpha=1. - center_momentum)
+
+        if sharpening:
+            # Apply sharpening to the target network outputs
+            # avoid collapse when the temperature is higher than 0.06
+            # temperature = 0.04
+            for param in self.target_encoder.parameters():
+                # param.data = torch.nn.functional.softmax(param.data / temperature, dim=-1)
+                param.data = param.data / (param.data.std() + 1e-6)
+
+    def forward(self, online_x, target_x, perturb_first=None, perturb_last=None):
+
+        online_y = self.online_encoder(online_x, perturb_first, perturb_last)  # Apply perturbations to the online encoder
+        online_q = self.predictor(online_y)
+
+        # forward pass for the target encoder (no perturbations)
+        with torch.no_grad(): 
+            target_y = self.target_encoder(target_x).detach()  # target encoder receives clean inputs
+
+        return online_q, target_y
+
+    def compute_zinc_representations(net, dataset, device, pooling='mean'):
+        """
+        Pre-computes the graph-level representations for the entire dataset.
+        Args:
+            net (torch.nn.Module): The encoder network.
+            dataset (Dataset): The dataset to process.
+            device (torch.device): Device to use for computation.
+            pooling (str): Pooling method to apply to node-level representations ('mean' or 'max').
+        """
+        net.eval()
+        reps = []
+        labels = []
+        pooling_layer = None
+    
+        # Define a pooling layer if needed
+        if pooling == 'mean':
+            pooling_layer = pyg_nn.global_mean_pool
+        elif pooling == 'max':
+            pooling_layer = pyg_nn.global_max_pool
+    
+        for data in dataset:
+            data = data.to(device)
+            with torch.no_grad():
+                node_reps = net(data)  # Node-level representations
+    
+                # Apply pooling to get a graph-level representation
+                if pooling_layer is not None:
+                    graph_rep = pooling_layer(node_reps, data.batch)  # Apply pooling
+                else:
+                    raise ValueError("Unsupported pooling type. Use 'mean' or 'max'.")
+    
+                reps.append(graph_rep)
+                labels.append(data.y.unsqueeze(1))  # Ensure labels are 2D for consistency
+    
+        # Concatenate all graph-level representations and labels
+        reps = torch.cat(reps, dim=0)
+        labels = torch.cat(labels, dim=0)
+    
+        return reps, labels
+
+    def compute_representations(net, dataset, device):
+        r"""Pre-computes the representations for the entire dataset.
+        """
+        net.eval()
+        reps = []
+        labels = []
+    
+        for data in dataset:
+            # forward
+            if isinstance(data, dict):
+                data = {k: v.to(device) if hasattr(v, 'to') else v for k, v in data.items()}
+                # data = data['valid']
+                # print(data.get('x', data.get('node_feat')))
+                # print(f"data: {data}")
+            else:
+                data = data.to(device)
+            with torch.no_grad():
+                reps.append(net(data))
+                # print(data.x)
+                labels.append(data.y)
+        reps = torch.cat(reps, dim=0)
+        labels = torch.cat(labels, dim=0)
+        return [reps, labels]
+
+class LaplaceGNN_v2(torch.nn.Module):
+    r""" LaplaceGNN: LaplaceGNN: Scalable Graph Learning through Spectral Bootstrapping and Adversarial Training
+    Args:
+        encoder (torch.nn.Module): First encoder network for augmentations (from module.py).
         augmentor (tuple): Tuple containing two augmentation functions.
         hidden_dim (int): Dimensionality of the hidden representation.
         predictor (torch.nn.Module): Predictor network used to predict the target projection from the online projection.
 
     .. note::
-        Both `encoder1` and `encoder2` must have a `reset_parameters` method, as the weights of the target network will
+        Encoder must have a `reset_parameters` method, as the weights of the target network will
         be initialized differently from the online network.
     """
     def __init__(self, encoder, predictor, augmentor):
@@ -99,17 +236,10 @@ class LaplaceGNN(torch.nn.Module):
 
         return online_q, target_y
     
-    def load_trained_encoder(encoder, ckpt_path, device):
-        r"""Utility for loading the trained encoder."""
-        checkpoint = torch.load(ckpt_path, map_location=device)
-        encoder.load_state_dict(checkpoint['model'], strict=True)
         return encoder.to(device)
     
     def compute_representations(net, dataset, device):
         r"""Pre-computes the representations for the entire dataset.
-
-        Returns:
-            [torch.Tensor, torch.Tensor]: Representations and labels.
         """
         net.eval()
         reps = []
