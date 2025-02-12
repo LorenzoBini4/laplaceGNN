76be3e0ce1443b7701739ff3269aa6f1a19bc1c2
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2025-02-12 14:15:50 +0100

    Update main.sh

--- ssl_adv_node/main.sh
+++ ssl_adv_node/main.sh
@@ -1,13 +1,13 @@
 # #### JOB ARRAY ###
-python -u run_adv_node.py --flagfile=config/coauthor-cs.cfg > laplacian.coauthor-cs.out 2> laplacian.coauthor-cs.err
-# python -u run_adv_node.py --flagfile=config/amazon-computers.cfg > laplacian.amazon-computers.out 2> laplacian.amazon-computers.err
-# python -u run_adv_node.py --flagfile=config/amazon-photos.cfg > laplacian.amazon-photos.out 2> laplacian.amazon-photos.err
-# python -u run_adv_node.py --flagfile=config/coauthor-physics.cfg > laplacian.coauthor-physics.out 2> laplacian.coauthor-physics.err
-# python -u run_adv_node.py --flagfile=config/wiki-cs.cfg > laplacian.wiki-cs.out 2> laplacian.wiki-cs.err
-# python -u run_adv_node.py --flagfile=config/ogbn-arxiv.cfg > laplacian.ogbn-arxiv.out 2> laplacian.ogbn-arxiv.err
-# python -u run_adv_node.py --flagfile=config/pubmed.cfg > laplacian.pubmed.out 2> laplacian.pubmed.err
-# python -u run_adv_node.py --flagfile=config/ogbn-papers.cfg > laplacian.ogbn-papers.out 2> laplacian.ogbn-papers.err
+python -u run_adv_node.py --flagfile=config/coauthor-cs.cfg > laplaceGNN.coauthor-cs.out 2> laplaceGNN.coauthor-cs.err
+# python -u run_adv_node.py --flagfile=config/amazon-computers.cfg > laplaceGNN.amazon-computers.out 2> laplaceGNN.amazon-computers.err
+# python -u run_adv_node.py --flagfile=config/amazon-photos.cfg > laplaceGNN.amazon-photos.out 2> laplaceGNN.amazon-photos.err
+# python -u run_adv_node.py --flagfile=config/coauthor-physics.cfg > laplaceGNN.coauthor-physics.out 2> laplaceGNN.coauthor-physics.err
+# python -u run_adv_node.py --flagfile=config/wiki-cs.cfg > laplaceGNN.wiki-cs.out 2> laplaceGNN.wiki-cs.err
+# python -u run_adv_node.py --flagfile=config/ogbn-arxiv.cfg > laplaceGNN.ogbn-arxiv.out 2> laplaceGNN.ogbn-arxiv.err
+# python -u run_adv_node.py --flagfile=config/pubmed.cfg > laplaceGNN.pubmed.out 2> laplaceGNN.pubmed.err
+# python -u run_adv_node.py --flagfile=config/ogbn-papers.cfg > laplaceGNN.ogbn-papers.out 2> laplaceGNN.ogbn-papers.err
 
 ################## TRANSFER LEARNING ##################
-# python -u run_adv_node_ppi.py --flagfile=config/ppi.cfg > laplacian-ppi.o 2> laplacian-ppi.e
-# python -u run_adv_node_ppi.py --flagfile=config/zinc.cfg > laplacian-zinc.o 2> laplacian-zinc.e
+# python -u run_adv_node_ppi.py --flagfile=config/ppi.cfg > laplaceGNN-ppi.o 2> laplaceGNN-ppi.e
+# python -u run_adv_node_ppi.py --flagfile=config/zinc.cfg > laplaceGNN-zinc.o 2> laplaceGNN-zinc.e
